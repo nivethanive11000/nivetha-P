90f5463c164c8b19ec13a96bb1c593f0d287f24f
nivethanive11000 <nivethanive11000@gmail.com>
2025-09-10 10:18:29 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int num1,num2,sum;
+printf("enter the two integers:");
+scanf("%d %d,&num1,&num2");
+sum= num1*num2;
+printf("the sum of %d and %d is:%d\n",num1,num2,sum);
+  return 0;
+}
